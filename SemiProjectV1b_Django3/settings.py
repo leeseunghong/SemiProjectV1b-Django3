fab24940d5695803cce00910b8a2a498995aa1f9
leeseunghong <skdl1256@naver.com>
2022-06-21 12:41:49 +0900

    Django3 app urls.py, views.py 수정 - home, member, board

--- SemiProjectV1b_Django3/settings.py
+++ SemiProjectV1b_Django3/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'home.apps.HomeConfig',
-    'mamber.apps.MemberConfig',
+    'member.apps.MemberConfig',
     'board.apps.BoardConfig',
 ]
 
