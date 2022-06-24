2c723dc2d8fdb0d97b2cd8fd07756b2cd5befe1b
leeseunghong <skdl1256@naver.com>
2022-06-24 15:20:46 +0900

    Django3 board 새글쓰기, 리스트 출력

--- board/views.py
+++ board/views.py
@@ -1,13 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+# 게시판 목록 보기 처리
+from board.models import board
+from member.models import Member
+
+
 def list(request):
-    return render(request, 'board/list.html')
+    bdlist = board.objects.values(
+        'id', 'title', 'userid', 'regdate', 'views')\
+        .order_by('-id')
 
+    context = {'bds': bdlist}
+    return render(request, 'board/list.html', context)
 
+# 게시판 본문 보기 처리
 def view(request):
     return render(request, 'board/view.html')
 
-
+# 게시판 글쓰기 처리
+# get : board/write.html
+# post : 작성한 글을 디비에 저장, board/list.html로 이동
 def write(request):
-    return render(request, 'board/write.html')
+    returnPage = 'board/write.html'
+    form = ''
+    error = ''
+
+    if request.method == 'GET':
+        pass
+    elif request.method == 'POST':
+        form = request.POST.dict()
+
+        #유효성 검사 1
+        if not (form['title'] and form['contents']):
+            error = '제목이나 본문을 작성하세요'
+        else:
+            # 입력한 게시글을 Board객체에 담음
+            bd = board(title=form['title'],
+                       contents=form['contents'],
+                       # 새글을 작성한 회원에 대한 정보는
+                       # 회원 테이블에 존재하는 회원아이디(id)를 조회해서
+                       # userid 속성에 저장
+                       userid=Member.objects.get(pk=form['memberid']))
+            bd.save()   # board객체에 담은 게시글을 테이블에 저장
+
+            return redirect('/list')
+
+    context = {'form': form, 'error': error}
+    return render(request, returnPage, context)
